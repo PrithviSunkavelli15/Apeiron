9e31882af4f7d57c6344a73a389dece85e11695c
Prithvi Sunkavelli <ps1439@scarletmail.rutgers.edu>
2026-03-26 20:06:09 -0400

    Updated Espresso after Dark 2

--- src/UpcomingEvents.js
+++ src/UpcomingEvents.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./UpcomingEvents.css";
 import landingImage from "./Events.png";
 import empireInIvoryFlyer from "./EmpireInIvory.JPG";
+import espressoAfterDarkFlyer from "./EspressoAfterDark2.jpeg";
 
 const UpcomingEvents = () => {
     const handleEmpireInIvoryClick = () => {
         window.open("https://flite.city/e/empire-in-ivory?t=you", "_blank");
+    };
+    const handleEspressoAfterDarkClick = () => {
+        window.open("https://flite.city/e/espresso-after-dark-ii", "_blank");
     };
 
     return (
@@ -19,6 +23,26 @@ const UpcomingEvents = () => {
                     loading="eager"
                     decoding="async"
                 />
+            </div>
+
+            {/* Espresso After Dark II Event */}
+            <div className="events-row">
+                <div className="events-left">
+                    <h2 className="event-title-line">
+                        <span>Espresso After Dark II</span>
+                    </h2>
+                    <div className="event-date">4/16/26</div>
+                </div>
+                <div className="events-right">
+                    <img
+                        src={espressoAfterDarkFlyer}
+                        alt="Espresso After Dark II"
+                        className="events-media event-flyer-clickable"
+                        onClick={handleEspressoAfterDarkClick}
+                        loading="lazy"
+                        decoding="async"
+                    />
+                </div>
             </div>
 
             {/* Empire in Ivory Event */}
